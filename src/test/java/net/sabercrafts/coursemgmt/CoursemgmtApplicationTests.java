84f50a3e7f4f40958134afa37103c8784ca7ffed
Saber <isaber23@outlook.com>
2020-05-25 18:15:53 +0000

    initial project

---
+++ src/test/java/net/sabercrafts/coursemgmt/CoursemgmtApplicationTests.java
@@ -0,0 +1,13 @@
+package net.sabercrafts.coursemgmt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoursemgmtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
